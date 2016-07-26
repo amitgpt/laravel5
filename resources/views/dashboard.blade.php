9dc14fe7390c2ff24cb2f3edf2118e25455a199d
amitgpt <amitgpt773@gmail.com>
2016-07-26 20:51:59 +0530

    changes in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
 		<div class="nav-wrapper container">
 		  <a id="logo-container" href="/" class="brand-logo white-text">Logo</a>
 		  <ul class="right hide-on-med-and-down">
-			<li><a href="/" class="white-text">Home</a></li> 
+			<li><a href="/" class="white-text">Homedsfhgjhxvhjchdsfdgvfk</a></li> 
 			<li><a href="/about" class="white-text">About</a></li>
 			<li><a href="/post" class="white-text">Sample Post</a></li>
 			<li><a href="/contact" class="white-text">Contact</a></li>
